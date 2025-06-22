900a2ced2a96d0febc7d35ae4441173f75b261e6
Uwais Shaikh <122542988+shaik-mohammed-uwais@users.noreply.github.com>
2025-06-22 12:39:38 +0530

    Update aiservice.js

--- src/services/aiservice.js
+++ src/services/aiservice.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-const API_BASE_URL = `${import.meta.env.VITE_BACKEND_URL}/api`;
+const API_BASE_URL = `${import.meta.env.VITE_BACKEND_URL}`;
 
 export default async function generateBlog(topic) {
   try {
-    const response = await axios.post(`${API_BASE_URL}/generate-blog`, {
+    const response = await axios.post(`${API_BASE_URL}/api/generate-blog`, {
       topic,
     });
 
